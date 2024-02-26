c9c418a201f4316c39e524541bfc35285848f32b
karllasnascimento <zup.knascimento@creditas.com>
2024-02-25 21:00:42 -0300

    chore: training multiple providers

--- providers/terraform.tf
+++ providers/terraform.tf
@@ -5,5 +5,17 @@ terraform {
       source = "hashicorp/aws"
       version = "~> 3.0"
     }
+    http = {
+        source = "hashicorp/http"
+        version = "2.1.0"
+    }
+    random = {
+        source = "hashicorp/random"
+        version = "3.1.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.1.0"
+    }
   }
 }
