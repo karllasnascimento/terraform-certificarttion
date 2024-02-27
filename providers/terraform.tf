124eaeb54fa35cca7705a562d17e1417fabea9b9
karllasnascimento <zup.knascimento@creditas.com>
2024-02-27 20:37:06 -0300

    chore: refactoring and using modules

--- providers/terraform.tf
+++ providers/terraform.tf
@@ -2,23 +2,23 @@ terraform {
   required_version = ">= 1.0.0"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 3.0"
     }
     http = {
-        source = "hashicorp/http"
-        version = "2.1.0"
+      source  = "hashicorp/http"
+      version = "2.1.0"
     }
     random = {
-        source = "hashicorp/random"
-        version = "3.1.0"
+      source  = "hashicorp/random"
+      version = "3.1.0"
     }
     local = {
       source  = "hashicorp/local"
       version = "2.1.0"
     }
     tls = {
-      source = "hashicorp/tls"
+      source  = "hashicorp/tls"
       version = "4.0.5"
     }
   }
