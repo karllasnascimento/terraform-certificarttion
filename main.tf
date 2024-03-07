2975ac58796dcdef199bcb9dbecf611fbcaa72fe
karllasnascimento <zup.knascimento@creditas.com>
2024-03-07 09:26:14 -0300

    chore: deepdive with local tags

--- main.tf
+++ main.tf
@@ -12,6 +12,30 @@ provider "aws" {
 
 }
 
+locals {
+  team = "eu-mesma"
+  application = "corp_api"
+  server_name = "ec2-estudy"
+}
+
+locals {
+  service_name = "Automation"
+  app_team     = "Cloud Team"
+  createdby    = "terraform"
+}
+
+locals {
+  # Common tags to be assigned to all resources
+  common_tags = {
+    Name      = local.server_name
+    Owner     = local.team
+    App       = local.application
+    Service   = local.service_name
+    AppTeam   = local.app_team
+    CreatedBy = local.createdby
+ } 
+}
+
 #Retrieve the list of AZs in the current AWS region
 data "aws_availability_zones" "available" {}
 data "aws_region" "current" {}
@@ -174,9 +198,7 @@ resource "aws_instance" "ubuntu_server" {
     ]
   }
 
-  tags = {
-    Name = "Ubuntu EC2 Server"
-  }
+  tags = local.common_tags
 
   lifecycle {
     ignore_changes = [security_groups]
